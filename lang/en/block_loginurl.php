d54efd5a221b1b426c13ae1f4919a600c80f8e8a
santosh K. Suwarnkar <santosh.s@redchipsolutions.com>
2016-04-08 15:38:04 +0530

    Adding Lang Folder
    Adding lang folder with language file.

---
+++ lang/en/block_loginurl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Strings for component 'block_loginurl', language 'en'
+ *
+ * @package   block_loginurl
+ * @copyright Redchip Solutions
+ */
+
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['loginurl:addinstance'] = 'Add a loginurl block';
+$string['loginurl:myaddinstance'] = 'Add a loginurl block to my moodle';
+$string['pluginname'] = 'Loginurl';
